46d7fb50e179ef070effae27c69143e06f1803b8
James Witkowski <jameswitkowski@James-Witkowskis-MacBook-Pro.local>
2016-01-13 14:12:23 -0700

    step #6 functions added

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var health = 100;
+var name = "Player01";
+var hits = 0;
 function slap() {
-    for ( health = 99; health > 0; health--){
+    for ( health = 100; health > 0; health--){
     }
 }
 
@@ -13,6 +15,19 @@ function update() {
 
 update();
 
+function punch() {
+    for (health = 100 - 5; health > 5; health = health - 5) { 
+            alert(health);
+    }
+}
+punch();
+
+function kick() {
+    for (health = 100 - 10; health > 10; health = health - 10) { 
+            alert(health);
+    }
+}
+kick();
 
 
 
