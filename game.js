108cffb2677efc88bcd11c67c019c485a8354c5c
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2016-01-14 16:10:36 -0700

    fixed panel to turn red.

--- game.js
+++ game.js
@@ -21,12 +21,20 @@ function kick() {
 }
 
 function upDate() {
-   var hp1 = document.getElementById("Health");
-   hp1.innerText = health.toString();
+   var np1 = document.getElementById("Name");
+   np1.innerText = name.toString(); 
    var hits1 = document.getElementById("Hits");
    hits1.innerText = hits.toString();
-   var np1 = document.getElementById("Name");
-   np1.innerText = name.toString();   
+   var hp1 = document.getElementById("Health");
+   hp1.innerText = health.toString();
+   if(health <= 20){
+		 document.getElementById("panel-header").classList.remove("panel-default")
+		 document.getElementById("panel-header").classList.add("panel-danger")
+	}  
+    
+   if (health <= 0){
+       alert("You Win!")
+   }
 }
 
 upDate();
