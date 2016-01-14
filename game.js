0791f24594b52ab484e8c156045976d6cd562d9d
James Witkowski <jameswitkowski@James-Witkowskis-MacBook-Pro.local>
2016-01-14 14:36:54 -0700

    Part 1 complete through testing phase

--- game.js
+++ game.js
@@ -2,48 +2,30 @@ var health = 100;
 var name = "Player01";
 var hits = 0;
 function slap() {
-    for ( health = 100; health > 0; health--){
-    }
+    health -= 1;
+    hits++;
+   upDate();
 }
-
-slap();
-
-function update() {
-   var x = document.getElementById("Health");
-   x.innerText =  'test';
-    if(health <= 0){
-		 document.getElementById("player-panel").classList.add("panel-danger")
-	}else{
-		 document.getElementById("player-panel").classList.remove("panel-danger")
-	}
-}
-
-update();
 
 function punch() {
-    for (health = 100 - 5; health > 5; health = health - 5) { 
-            alert(health);
-    }
+    health -= 5;
+    hits++;
+    upDate();
 }
-punch();
 
 function kick() {
-    for (health = 100 - 10; health > 10; health = health - 10) { 
-            alert(health);
-    }
+    health -= 10;
+    hits++;
+    upDate();
 }
-kick();
 
-function Item(name,modifier,description){
-    this.name = name,
-    this.modifier = modifier,
-    this.description = description
-    this.draw = function(){
-    
-	}
+function upDate() {
+   var hp1 = document.getElementById("Health");
+   hp1.innerText = health.toString();
+   var hits1 = document.getElementById("Hits");
+   hits1.innerText = hits.toString();
+   var np1 = document.getElementById("Name");
+   np1.innerText = name.toString();   
 }
-var items{
-    shield:new Item("Shield", 0.3, "This is an awesome shield"),
-    helmet:new Item("Helemt", 0.2, "This is a heavy helmet"),
-    armor:new Item("Armor", 0.1, "This is light-weight metal"),
-}
+
+upDate();
